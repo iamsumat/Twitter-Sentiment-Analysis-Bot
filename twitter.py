573dddad36970d2a42129ae28ac9c94fd453f324
Sumat <simbed.aqua@gmail.com>
2020-02-06 18:15:35 +0530

    Reformat code, added TwitterAuth class

--- twitter.py
+++ twitter.py
@@ -6,23 +6,38 @@ from tweepy import Stream
 
 import credentials
 
-class Twitter_Streamer():
-    # Class for streaming and processing live streams.
+class Twitter_Authenticator(): # TWITTER AUTHENTICATOR
+    '''
+    Class for authenticating Twitter app.
+    '''
+
+    def authenticate_app(self):
+        auth = OAuthHandler(credentials.CONSUMER_KEY, credentials.CONSUMER_SECRET)
+        auth.set_access_token(credentials.API_KEY, credentials.API_SECRET)
+        return auth
+
+
+class Twitter_Streamer(): # TWITTER STREAMER
+    '''
+    Class for streaming and processing live streams.
+    '''
+
     def __init__(self):
+        self.twitter_authenticate = Twitter_Authenticator()
         pass
 
     def stream_tweets(self, json_filename, hash_list):
         # This handles Twitter authentication and the connection to Twitter API.
-        listener = StdOutListener()
-        auth = OAuthHandler(credentials.CONSUMER_KEY, credentials.CONSUMER_SECRET)
-        auth.set_access_token(credentials.API_KEY, credentials.API_SECRET)
-
+        listener = TwitterListener(json_filename)
+        auth = self.twitter_authenticate.authenticate_app()
         stream = Stream(auth, listener)
         stream.filter(track=hash_list)
 
 
-class StdOutListener(StreamListener):
-    # Standard listener class that prints recieved tweets to StdOut.
+class TwitterListener(StreamListener): # TWITTER STREAM LISTENER
+    '''
+    Standard listener class that prints recieved tweets to StdOut.
+    '''
 
     def __init__(self, json_filename):
         self.json_filename = json_filename
